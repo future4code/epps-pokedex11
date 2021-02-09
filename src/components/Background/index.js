cfaafb4ce464e85efb21bd1f187f8938b4d3d54e
fsantos22 <22fsantos@gmail.com>
2021-02-08 23:29:57 -0300

    Background adicionado

---
+++ src/components/Background/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Background } from './styled';
+
+const Backgorund = (props) => {
+    return (
+        <Background>
+            
+        </Background>
+    )
+}
+
+export default Backgorund
---
+++ src/components/Background/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+
+  background-image: url(${props.image});
+  width: 100vw;
+  height: 100vh;
+`;
