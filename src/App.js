805b70387b4133059bfc31c81a011b69830c2a81
fsantos22 <22fsantos@gmail.com>
2021-02-08 23:24:16 -0300

    Header e Card criados

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import Header from "./components/Header";
+import styled from "styled-components";
+
+import bg from "./images/pokemon-pattern.jpg";
+import Card from "./components/Card";
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+
+  background-image: url(${bg});
+  width: 100vw;
+  height: 100vh;
+`;
+
 export default function App() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
+  return <div>Hello World</div>;
 }
