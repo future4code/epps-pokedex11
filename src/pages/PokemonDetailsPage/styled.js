8c8a964f74ce5fb9272e2ec2609db7cddd0a6268
shidoshi93 <dann.ribeiroo@gmail.com>
2021-02-12 14:30:42 +0000

    Aplicado estilização na página de detelhes.

--- src/pages/PokemonDetailsPage/styled.js
+++ src/pages/PokemonDetailsPage/styled.js
@@ -28,51 +28,87 @@ export const ContainerContentPokemon = styled.div`
   height: 60vh;
   flex-wrap: wrap;
   gap: 25px;
+  color: #ffcb05;
+
+  div, img{
+    border-radius: 10px;
+  }
 
   h2 {
     text-align: center;
+    font-family: "PokemonFont";
+    letter-spacing: 3px;
+    -webkit-text-stroke-width: 0.7px;
+    -webkit-text-stroke-color: #003a70;
+    margin: 0;
   }
-  h2,
+  
   p {
     margin: 30px;
   }
+
   .imgs-Pokemon {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
 
     img {
-      background-color: #f2f2f2;
+      background-color: #cc0000;
       width: 150px;
+
+      &:hover{
+        box-shadow: 2px 2px 5px black;
+      }
     }
 
     @media screen and (max-width: 619px) {
       flex-direction: row;
     }
   }
+
   .stats {
-    background-color: #f2f2f2;
+    background-color: #cc0000;
     width: 300px;
+
+    &:hover{
+        box-shadow: 2px 2px 5px black;
+      }
   }
+
   .types-And-Moves {
     display: flex;
     justify-content: space-between;
     flex-direction: column;
     width: 300px;
   }
+
   .types {
     display: flex;
     justify-content: space-evenly;
     align-items: center;
-    background-color: #f2f2f2;
+    background-color: #cc0000;
     height: 50px;
+
+    &:hover{
+        box-shadow: 2px 2px 5px black;
+      }
   }
+
   .moves {
     height: 320px;
-    background-color: #f2f2f2;
+    background-color: #cc0000;
+
+    h2{
+      margin: 0;
+    }
+
     div {
       height: 200px;
       overflow-y: auto;
     }
+
+    &:hover{
+        box-shadow: 2px 2px 5px black;
+      }
   }
 `;
